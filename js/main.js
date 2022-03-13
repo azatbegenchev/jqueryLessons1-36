1d63d8bc80b7c4c1e962a1ace55edb778ad72636
Azat <azat.begenchev@yandex.ru>
2022-03-13 22:51:01 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+   var tagP = $('.mainText p');
+   tagP.hide(2000).text('Your feauture is important for us').show(2000);
+
+   var icons = $('.icons > div');
+   icons.hide(3000).show(2000);
+})
